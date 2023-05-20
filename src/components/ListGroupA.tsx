57a6b055917963bdaa845e7a71121dab3dff7731
sudersoncispl <67859927+sudersoncispl@users.noreply.github.com>
2023-05-20 18:22:29 +0530

    Added new countries to ListGroupA.tsx

--- src/components/ListGroupA.tsx
+++ src/components/ListGroupA.tsx
@@ -1,7 +1,7 @@
 import ListGroup from './ListGroup';
 
 function ListGroupA() {
-  let items = ['India', 'Nepal', 'Bhutan', 'SriLanka', 'Pakistan', 'Chaina'];
+  let items = ['India', 'Nepal', 'Bhutan', 'SriLanka', 'Pakistan', 'Chaina','Indronesia','Australia','Japan'];
 
   function onSelectItem(item: string) {
     console.log(item);
