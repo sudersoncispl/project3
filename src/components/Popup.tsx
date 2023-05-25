fae713670471b3d0d5b64d6bdb2e37baad23efd3
Suderson <suderson.halder@codeclouds.com>
2023-05-25 18:47:12 +0530

    Popup not displaying

--- src/components/Popup.tsx
+++ src/components/Popup.tsx
@@ -1,46 +1,60 @@
 import { useState } from 'react';
 
 function Popup() {
+  //console.log(showPOp);
   let [showPOp, setShowPop] = useState(false);
-
   return (
-    <div
-      className='modal'
-      style="{showPOp && { display: 'block', position: 'initial' }}"
-      tabIndex={1}
-    >
-      <div className='modal-dialog'>
-        <div className='modal-content'>
-          <div className='modal-header'>
-            <h5 className='modal-title'>Modal title</h5>
-            <button
-              type='button'
-              className='btn-close'
-              data-bs-dismiss='modal'
-              aria-label='Close'
-              onClick={() => {
-                setShowPop(false);
-              }}
-            ></button>
-          </div>
-          <div className='modal-body'>
-            <p>Modal body text goes here.</p>
-          </div>
-          <div className='modal-footer'>
-            <button
-              type='button'
-              className='btn btn-secondary'
-              data-bs-dismiss='modal'
-            >
-              Close
-            </button>
-            <button type='button' className='btn btn-primary'>
-              Save changes
-            </button>
+    <>
+      <button
+        type='button'
+        className='btn btn-primary'
+        onClick={() => {
+          setShowPop(true);
+          console.log('On true', showPOp);
+        }}
+      >
+        Primary
+      </button>
+
+      {showPOp == true ? (
+        <div className='modal' tabIndex={1}>
+          <div className='modal-dialog'>
+            <div className='modal-content'>
+              <div className='modal-header'>
+                <h5 className='modal-title'>Modal title</h5>
+                <button
+                  type='button'
+                  className='btn-close'
+                  data-bs-dismiss='modal'
+                  aria-label='Close'
+                  onClick={() => {
+                    setShowPop(false);
+                    console.log('On false', showPOp);
+                  }}
+                ></button>
+              </div>
+              <div className='modal-body'>
+                <p>Modal body text goes here.</p>
+              </div>
+              <div className='modal-footer'>
+                <button
+                  type='button'
+                  className='btn btn-secondary'
+                  data-bs-dismiss='modal'
+                >
+                  Close
+                </button>
+                <button type='button' className='btn btn-primary'>
+                  Save changes
+                </button>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
+      ) : (
+        ''
+      )}
+    </>
   );
 }
 
